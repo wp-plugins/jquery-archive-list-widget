b5bee048b277026e8c25cd2bb26f0b7fb77fd7cb
skatox <skatox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-26 22:27:14 +0000

    Uploading 1.0 stable version
    git-svn-id: https://plugins.svn.wordpress.org/jquery-archive-list-widget/trunk@315978 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery-archive-list-widget.php
+++ jquery-archive-list-widget.php
@@ -4,10 +4,10 @@
  Plugin URI: http://skatox.com/blog/2009/12/26/jquery-archive-list-widget/
  Description: A simple jQuery widget for displaying an archive list with some effects (inspired by Collapsible Archive Widget)
  Author: Miguel Useche
- Version: 0.1.2
+ Version: 1.0.0
  Author URI: http://skatox.com/
 
-    Copyright 2009  Miguel Useche  (email : migueluseche@skatox.com)
+    Copyright 2009-2010  Miguel Useche  (email : migueluseche@skatox.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -24,77 +24,199 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+$jal_dir = preg_replace("/^.*[\/\\\]/", "", dirname(__FILE__));
+define ("JAL_DIR", "/wp-content/plugins/" . $jal_dir);
+
+/**
+ * Loads plugin's options, interprets it for internal use
+ * @return array of custom options
+*/
+function aux_load_options(){
+	//Loads options
+	$options = get_option('jquery_archive_list_widget');
+	$jal_options= array();
+	$jal_options['showpost'] = 0;
+	$jal_options['showcount'] = $options['showcount'];
+	$jal_options['month_format'] = $options['month_format'];
+	$jal_options['title'] = $options['title'];
+	
+	if($options['showpost']=='true')
+		$jal_options['showpost']= 1;	
+
+	switch ($options['symbol']){
+		case '0':
+					$jal_options['ex_sym']=' ';
+					$jal_options['con_sym']=' ';
+					break;
+		case '1':
+					$jal_options['ex_sym']='►';
+					$jal_options['con_sym']='▼';
+					break;
+		case '2':
+					$jal_options['ex_sym']='(+)';
+					$jal_options['con_sym']='(-)';
+					break;
+		case '3':
+					$jal_options['ex_sym']='[+]';
+					$jal_options['con_sym']='[-]';
+					break;
+		default:
+					$jal_options['ex_sym']='>';
+					$jal_options['con_sym']='v';
+					break;
+	}
+	
+	switch ($options['effect']){
+		case 'slide':
+					$jal_options['fx_in']='slideDown';
+					$jal_options['fx_out']='slideUp';
+					break;
+		case 'fade':
+					$jal_options['fx_in']='fadeIn';
+					$jal_options['fx_out']='fadeOut';
+					break;
+	}
+	return $jal_options;
+}
+
 function aux_get_years(){
 	global $wpdb;
+	
+	$sql = "SELECT DISTINCT YEAR(post_date) AS `year`, count(ID) as posts ";
+	$sql .="FROM {$wpdb->posts} WHERE {$wpdb->posts}.post_status = 'publish' AND {$wpdb->posts}.post_type = 'post' ";
+	$sql .="GROUP BY YEAR(post_date) ORDER BY post_date DESC";
 
-	$years = $wpdb->get_results("SELECT DISTINCT YEAR(post_date) AS `year`, count(ID) as posts"
-	." FROM $wpdb->posts WHERE $wpdb->posts.post_status = 'publish' AND $wpdb->posts.post_type = 'post'"
-	." GROUP BY YEAR(post_date) ORDER BY post_date DESC");
-
-	return $years;
+	return $wpdb->get_results($sql);
 }
 
 function aux_get_months($year){
 	global $wpdb;
 
-	$months = $wpdb->get_results("SELECT DISTINCT YEAR(post_date) AS `year`, MONTH(post_date) AS `month`, count(ID) as posts"
-	." FROM $wpdb->posts WHERE $wpdb->posts.post_status = 'publish' AND $wpdb->posts.post_type = 'post' AND YEAR(post_date) = {$year}"
-	." GROUP BY YEAR(post_date), MONTH(post_date) ORDER BY post_date DESC");
+	$sql = "SELECT DISTINCT YEAR(post_date) AS `year`, MONTH(post_date) AS `month`, count(ID) as posts ";
+	$sql.= "FROM {$wpdb->posts} WHERE {$wpdb->posts}.post_status = 'publish' AND {$wpdb->posts}.post_type = 'post' AND YEAR(post_date) = {$year} ";
+	$sql.= "GROUP BY YEAR(post_date), MONTH(post_date) ORDER BY post_date DESC";
 
-	return $months;
+	return $wpdb->get_results($sql);
 }
 
 function aux_get_posts($year,$month){
 	global $wpdb;
 
-	if (empty($year) || empty($month)) return null;
+	if (empty($year) || empty($month))
+		return null;
 
-   	$posts = $wpdb->get_results("SELECT ID, post_title, post_name FROM $wpdb->posts WHERE $wpdb->posts.post_status = 'publish' "
-	." AND $wpdb->posts.post_type = 'post' AND YEAR(post_date) = {$year} AND MONTH(post_date) = {$month} ORDER BY post_date DESC");
-
-	return $posts;
+   	$sql = "SELECT ID, post_title, post_name FROM {$wpdb->posts} ";
+	$sql .="WHERE {$wpdb->posts}.post_status = 'publish' AND {$wpdb->posts}.post_type = 'post' ";
+	$sql .="AND YEAR(post_date) = {$year} AND MONTH(post_date)={$month} ORDER BY post_date DESC";
+	
+	return $wpdb->get_results($sql);
 }
 
-function aux_print_js_code($ex_sym, $con_sym,$effect_in,$effect_out,$showpost=false){
-	echo "<script type=\"text/javascript\">\n\t";
-	echo "jQuery(document).ready(function() {\n\t\t";
-	echo "jQuery('.jaw_months').hide();\n\t\t";
-	echo "jQuery('p.jaw_years').bind(\"click\", function(){\n\t\t\t";
-	echo "jQuery('.jaw_months').{$effect_out}('fast');\n\t\t\t";
-	echo "jQuery('.jaw_symbol').html('{$ex_sym}');\n\t\t\t";
-	echo "if(jQuery(this).parent().find('.jaw_months').is(':hidden')){\n\t\t\t\t";
-	echo "jQuery(this).parent().find('.jaw_symbol').html('{$con_sym}');\n\t\t\t\t";
-    echo "jQuery(this).parent().find('.jaw_months').{$effect_in}('fast');\n\t\t\t\t";
-	echo "}\n\t\t\telse{\n\t\t\t\t";
-  	echo "jQuery(this).parent().find('.jaw_months').{$effect_out}('fast');\n\t\t\t";
-	echo "}\n\t\t";
-	echo "});\n\t";
-	//Javascript code for expanding or hiding posts entries
-	if($showpost){ 
-		echo "jQuery('li.jaw_months').bind(\"click\", function(){\n\t\t\t";
-		echo "jQuery('.jaw_posts').{$effect_out}('normal');\n\t\t\t";
-		echo "jQuery('.jaw_symbol').html('{$ex_sym}');\n\t\t\t";
-		echo "if(jQuery(this).find('.jaw_posts').is(':hidden')){\n\t\t\t\t";
-        echo "jQuery(this).find('.jaw_posts').{$effect_in}('normal');\n\t\t\t\t";
-		echo "jQuery(this).find('.jaw_symbol').html('{$con_sym}');\n\t\t\t";
-		echo "}\n\t\t\telse{\n\t\t\t\t";
-  		echo "jQuery(this).find('.jaw_posts').{$effect_out}('normal');\n\t\t\t";
-		echo "}\n\t\t";
-		echo "});\n\t";
+/**
+ * Builds Javascript's source url with custom parameters to generate dynamic js code
+*/
+function aux_build_js_url() {
+	$options = aux_load_options();
+	
+	$url = '/jquery-archive-list.js.php';
+	$url.= '?ex_sym='.rawurlencode($options['ex_sym']).'&con_sym='.rawurlencode($options['con_sym']);
+	$url.= '&fx_out='.$options['fx_out'].'&fx_in='.$options['fx_in'].'&showpost='.$options['showpost'];
+	
+	return $url;
+}
+
+/**
+ * Builds archive list's HTML code
+*/
+function aux_build_html_code($jal_options) {
+	global $wp_locale;
+	
+	$years = aux_get_years();
+	$html= '<ul>';
+
+	//Prints Years
+	for ($i=0;$x<count($years[$i]);$i++){
+		$html.= "\n<li class=\"jaw_years\"><a class=\"jaw_years\" href=\"#jal\">";
+		$html.= '<span class="jaw_symbol">'.htmlspecialchars($jal_options['ex_sym'])."</span>&nbsp;{$years[$i]->year}";
+	
+		//Prints number of post_date	
+		if($jal_options['showcount']=='true') 
+			$html.= " ({$years[$i]->posts})";
+		
+		$html.= '</a><ul>';
+		
+		//Prints Months
+		$months = aux_get_months($years[$i]->year);
+
+		foreach ($months as $month){
+			if($jal_options['showpost']) 
+				$month_url ='#jal';
+			else 
+				$month_url = get_month_link($years[$i]->year, $month->month);
+				
+			$html.= "\n\t<li class=\"jaw_months\" style=\"display:none;\"><a class=\"jaw_months\" href=\"{$month_url}\">";
+			
+			if($jal_options['showpost'])
+				$html.= '<span class="jaw_symbol">'.htmlspecialchars($jal_options['ex_sym']).'&nbsp;</span>';
+			
+			//Prints month according to selected format
+			switch($jal_options['month_format']) {
+				case 'short':
+					$html.= $wp_locale->get_month_abbrev($wp_locale->get_month($month->month));
+					break;
+				case 'number':
+					if($month->month<10) $html.= '0'.$month->month;
+					else $html.= $month->month;
+					break;
+				default:
+					$html.= $wp_locale->get_month($month->month);
+					break;
+			}
+					
+			if($jal_options['showcount'])
+				$html.= " ({$month->posts})";
+			$html.= '</a>'; 
+		      
+			if($jal_options['showpost']){
+				$html.= '<ul>';
+				$posts= aux_get_posts($years[$i]->year, $month->month);
+				foreach ($posts as $post){
+					$html.= "\n\t\t";
+					$html.= '<li class="jaw_posts" style="display:none;">';
+					$html.= '<a href="'.get_permalink($post->ID).'">'.$post->post_title.'</a></li>';
+				}
+				$html.= '</ul>';
+			}				
+			$html.= '</li>';
+		}
+		$html.= '</ul></li>';
 	}
-	echo "\n});\n";
-	echo '</script>';
+	$html.= '</ul>';
+	
+	return $html;
 }
 
-
+/**
+ * Function wich filters any [jQuery Archive List] text inside post to display archive list
+*/
+function widget_filter_jquery_archives($content){
+	$jal_options = aux_load_options();
+	$content=str_ireplace('[jQuery Archive List]',aux_build_html_code($jal_options),$content);
+    return $content;
+}
+	
+/**
+ * Function wich displays configuration form used at widgets area at Administration page
+*/
 function widget_display_jquery_archives_control(){	
 	$options = get_option('jquery_archive_list_widget');
-
+	
 	//If submit button was pressed the variables are loaded by POST
 	if ($_POST['jquery_archives_widget_submit']){	
 		$options['title'] = stripslashes($_POST['jquery_archives_widget_title']);
 		$options['symbol'] = $_POST['jquery_archives_widget_symbol'];
 		$options['effect'] = stripslashes($_POST['jquery_archives_widget_effect']);
+		$options['month_format'] = stripslashes($_POST['jquery_archives_widget_format']);
 		
 		if (isset($_POST['jquery_archives_widget_showpost']))
 			$options['showpost'] = true; 
@@ -105,135 +227,86 @@ function widget_display_jquery_archives_control(){
 			$options['showcount'] = true; 
 		else 
 			$options['showcount'] = false;
-	}
-	//Update options in the Wordpress database
-	update_option('jquery_archive_list_widget', $options);	
-?>          
+			
+		//Update options in the Wordpress database
+		update_option('jquery_archive_list_widget', $options);	
+	}?>
+	
     <dl>
-        <dt><strong>Title</strong></dt>
+        <dt><strong><?php _e('Title') ?></strong></dt>
         <dd>
             <input name="jquery_archives_widget_title" type="text" value="<?php echo $options['title']; ?>" />
         </dd>
-        <dt><strong>Trigger Symbol</strong></dt>
+        <dt><strong><?php _e('Trigger Symbol','jalw_i18n') ?></strong></dt>
         <dd>
-            <input id="jquery_archives_widget_symbol_0" name="jquery_archives_widget_symbol" type="radio" value="0" <?php if($options['symbol']=='0')  echo 'checked="checked"' ?> />
-	    <label for="jquery_archives_widget_symbol_0" />► ▼</label>
+			<select id="jquery_archives_widget_symbol" name="jquery_archives_widget_symbol">
+				<option value="0"  <?php if($options['symbol']=='0')  echo 'selected="selected"' ?> ><?php _e('Empty Space','jalw_i18n') ?></option>
+				<option value="1"  <?php if($options['symbol']=='1')  echo 'selected="selected"' ?> >► ▼</option>
+				<option value="2"  <?php if($options['symbol']=='2')  echo 'selected="selected"' ?> >(+) (-)</option>
+				<option value="3"  <?php if($options['symbol']=='3')  echo 'selected="selected"' ?> >[+] [-]</option>
+			</select>
         </dd>
-	<dd>
-            <input id="jquery_archives_widget_symbol_1" name="jquery_archives_widget_symbol" type="radio" value="1" <?php if($options['symbol']=='1') echo 'checked="checked"' ?> />
-	    <label for="jquery_archives_widget_symbol_1" />+ -</label>
-        </dd>
-	<dd>
-            <input id="jquery_archives_widget_symbol_2" name="jquery_archives_widget_symbol" type="radio" value="2" <?php if($options['symbol']=='2') echo 'checked="checked"' ?> />
-	    <label for="jquery_archives_widget_symbol_2" />[+] [-]</label>
-        </dd>
-      <dt><strong>jQuery Effect</strong></dt>
+		<dt><strong><?php _e('Effect','jalw_i18n')?></strong></dt>
         <dd>
             <select id="jquery_archives_widget_effect" name="jquery_archives_widget_effect">
-		  <option value="slide"  <?php if($options['effect']=='slide')  echo 'selected="selected"' ?> >slideUp/slideDown</option>
-		  <option value="fade" <?php if($options['effect']=='fade')  echo 'selected="selected"' ?> >fadeIn/fadeOut</option>
-	    </select>
+				<option value="slide"  <?php if($options['effect']=='slide')  echo 'selected="selected"' ?> ><?php _e('Slide (Accordion)','jalw_i18n') ?></option>
+				<option value="fade" <?php if($options['effect']=='fade')  echo 'selected="selected"' ?> ><?php _e('Fade','jalw_i18n') ?></option>
+			</select>
         </dd>
-      <dt><strong>Extra options</strong></dt>
+		<dt><strong><?php _e('Month Format','jalw_i18n') ?></strong></dt>
+		<dd>
+			<select id="jquery_archives_widget_format" name="jquery_archives_widget_format">
+				<option value="full" <?php if($options['month_format']=='full') echo 'selected="selected"'?> ><?php _e('Full Name (January)','jalw_i18n')?></option>
+				<option value="short" <?php if($options['month_format']=='short') echo 'selected="selected"'?> ><?php _e('Short Name (Jan)','jalw_i18n') ?></option>
+				<option value="number" <?php if($options['month_format']=='number') echo 'selected="selected"' ?> ><?php _e('Number (01)','jalw_i18n') ?></option>
+			</select>
+					
+		</dd>
+		<dt><strong><?php _e('Extra options','jalw_i18n') ?></strong></dt>
         <dd>
             <input id="jquery_archives_widget_showcount" name="jquery_archives_widget_showcount" type="checkbox" <?php if($options['showcount'] ) echo 'checked="checked"' ?> />
-	    <label for="jquery_archives_widget_showcount" />Show number of posts</label>
-	</dd>
-	<dd>
+			<?php _e('Show number of posts','jalw_i18n') ?>
+		</dd>
+		<dd>
 	    <input id="jquery_archives_widget_showpost" name="jquery_archives_widget_showpost" type="checkbox" <?php if($options['showpost'] )  echo 'checked="checked"' ?> />
-	    <label for="jquery_archives_widget_showpost" />Show posts under months</label>
+			<?php _e('Show posts under months','jalw_i18n') ?>
         </dd>
-    </dl> 
+	</dl> 
     <input type="hidden" name="jquery_archives_widget_submit" value="1" />
 <?php
 }
 
+/**
+ * Displays the archive list
+*/
 function widget_display_jQuery_archives($args) {
-	
 	extract($args);
-	global $wp_locale;
-
-	//Loads jQuery library
-	if (function_exists('wp_enqueue_script') && !is_admin()) 
-		wp_enqueue_script('jquery');
+	$jal_options = aux_load_options();
 	
-
-	//Loads options
-	$options = get_option('jquery_archive_list_widget');
-	$years = aux_get_years();		
-	$ex_sym="";$con_sym="";
-	$showpost=false;
-
-	if($options['showpost']=='true') 
-		$showpost=true;
-
-	switch ($options['symbol']){
-		case '0': $ex_sym='►';$con_sym='▼';break;
-		case '1': $ex_sym='+';$con_sym='-';break;
-		case '2': $ex_sym='[+]';$con_sym='[-]';break;
-	}
-
-	switch ($options['effect']){
-		case 'slide': $effect_in='slideDown';$effect_out='slideUp';break;
-		case 'fade': $effect_in='fadeIn';$effect_out='fadeOut';break;
-	}
-
-	//Prints widget title
 	echo $before_widget;
 	echo $before_title;
-	echo $options['title'];
+	echo $jal_options['title'];
 	echo $after_title;
-
-	echo '<ul>';
-	//Prints Years
-	for ($i=0;$x<count($years[$i]);$i++){
-		echo "\n<li ><p class=\"jaw_years\"><a href=\"#nolink\"><span class=\"jaw_symbol\">{$ex_sym}&nbsp;</span>{$years[$i]->year}";
-	
-		//Prints number of post_date	
-		if($options['showcount']=='true') 
-				echo " ({$years[$i]->posts})";
-		
-		echo '</a></p><ul>';
-		$months = aux_get_months($years[$i]->year);
-		
-		//Prints Months
-		foreach ($months as $month){
-			
-			if($showpost) 
-				$month_url ='#nolink';
-			else 
-				$month_url = get_month_link($years[$i]->year, $month->month);
-
-			echo "\n\t<li class=\"jaw_months\" style=\"display:none;\"><p><a href=\"{$month_url}\">";
-			if($options['showpost']=='true') echo "<span class=\"jaw_symbol\">{$ex_sym}&nbsp;</span>";
-			echo $wp_locale->get_month($month->month);
-			if($options['showcount']=='true') echo " ({$month->posts})";
-			echo '</a></p>'; 
-		      
-			if($showpost){
-				echo '<ul>';
-				$posts=aux_get_posts($years[$i]->year, $month->month);
-				foreach ($posts as $post){
-					echo "\n\t\t";
-					echo '<li class="jaw_posts" style="display:none;"><a href="'.get_permalink($post->ID).'">'.$post->post_title.'</a></li>';
-				}
-				echo '</ul>';
-			}			
-			
-			echo '</li>';
-		}
-		echo '</ul></li>';
-	}
-	echo '</ul>';
-	
-	//Prints javascript code (expand symbol, contraction symbol, jquery effect for entrace, jquery effect for output, flag for show post)
-	aux_print_js_code($ex_sym,$con_sym,$effect_in, $effect_out,$showpost);
+	echo aux_build_html_code($jal_options );
 	echo $after_widget;
 }
 
 function widget_display_jQuery_archives_init(){
-	register_sidebar_widget(__('jQuery Archive List Widget'), 'widget_display_jQuery_archives');
-	register_widget_control(__('jQuery Archive List Widget'),'widget_display_jQuery_archives_control');
+	//Includes translation file
+	if (function_exists("load_plugin_textdomain")) {
+		load_plugin_textdomain('jalw_i18n', JAL_DIR . "/lang/");
+	}
+	
+	//Includes dynamic js script and loads jquery if it is not loaded
+	if (function_exists("wp_enqueue_script") && !is_admin()) {
+		$js_url = aux_build_js_url();
+		wp_enqueue_script('jquery_archive_list', get_option("siteurl") . JAL_DIR . $js_url, array('jquery'));
+	}
+	
+	//register this plugin
+	register_sidebar_widget('jQuery Archive List Widget', 'widget_display_jQuery_archives');
+	register_widget_control('jQuery Archive List Widget','widget_display_jQuery_archives_control');
 }
 
-add_action("plugins_loaded", "widget_display_jQuery_archives_init");
+add_action('init','widget_display_jQuery_archives_init');
+add_filter('the_content', 'widget_filter_jquery_archives');
--- readme.txt
+++ readme.txt
@@ -3,36 +3,63 @@ Contributors: Miguel Useche
 Donate link: http://skatox.com/blog/2009/12/26/jquery-archive-list-widget/
 Tags: jquery, ajax, javacript, collapse, collapsible, archive, collapsible archive, widget
 Requires at least: 2.8
-Tested up to: 2.9
-Stable tag: 0.1.2
+Tested up to: 3.0.1
+Stable tag: 1.0.0
 
-A simple jQuery widget for displaying an archive list with some effects (inspired by Collapsible Archive Widget)
+A simple jQuery widget (can be called from posts) for displaying an archive list with some effects.
 
 == Description ==
 
-This simple plugin is a widget that displays a collapsible archives list in your sidebar by using jQuery.
+This plugin provides a widget and a filter to display a collapsible archive list in your sidebar or posts by using jQuery.
 
 == Installation ==
 
 1. Make a directory `jquery-archive-list-widget` under `/wp-content/plugins/`
 1. Upload `jquery-archive-list-widget.php` to the `/wp-content/plugins/jquery-archive-list-widget/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
-1. Use your 'Presentation'/'Sidebar Widgets' settings to drag and configure
+1. Use your 'Presentation'/'Sidebar Widgets' settings to drag and configure, if you want to display it inside a post then write [jQuery Archive List] and save.
 
 == Configuration ==
 
 * Title: the title of the widget
 * Trigger Symbol: The characters to be displayed as a bullet
 * jQuery Effect: The jQuery effect to be used.
+* Month Format: Display each post's title under the months.
 * Show number of posts: Show how many post are published in the year or in the month
 * Show posts under months: Display each post's title under the months.
 
-== TODO ==
+== Frequently Asked Questions ==
 
-* Add support for more effects.
-* Support for images as bullets.
-* More customization options.
+= How I can send you a translation? =
+
+Send me the translated .mo file to migueluseche(a)skatox.com and indicate the language, I can read english or spanish,
+so please write me on these languages.
+
+= Can I use images as bullets or trigger symbols? =
+
+Yes, select 'Empty Space' as trigger symbol and Save, then you can add any custom background using CSS
+to 'jaw_symbol' class or 'jaw_symbol' and 'expanded' for expanded items.
+
+= Can I show this list inside posts? =
+
+Yes, only write [jQuery Archive List] anywhere inside a post or page's contest and it will be replaced for
+the archive list when rendering the content.
+
+= How I contribute to this plugin? =
+
+By using it, recommending it to other users, giving it 5 starts at this plugin's wordpress page, suggesting features
+or coding new features and finally by DONATING using this plugin's website donate link.
 
 == Change Log ==
 
-* 26-Aug-2009: Initial version
+= 1.0 =
+* Added support for month's format
+* Now the jquery archive list can be printed from a post, just write [jQuery Archive List] anywhere inside the post.
+* Added support for i18n, so you can translate widget configuration's text to your language.
+* Separed JS code from HTML code, so browsers should cache JS content for faster processing. 
+* Automatic loading of jQuery JS library.
+* Almost all code were rewritten for better maintainer and easy way to add new features.
+* Improved code to be more Wordpress compatible.
+
+= 0.1.3 ==
+* Initial public version.
