c011123ac6e3f4b5b9d727b30703a8985033b0fb
skatox <skatox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-11 18:20:30 +0000

    Improved js default event, added slovak translation, fixed year bug
    git-svn-id: https://plugins.svn.wordpress.org/jquery-archive-list-widget/tags/1.2.1@436441 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ jquery-archive-list.js.php
@@ -0,0 +1,34 @@
+<?php
+	$ex_sym = rawurldecode($_GET['ex_sym']);
+	$con_sym= rawurldecode($_GET['con_sym']);
+	$fx_in= $_GET['fx_in'];
+	$fx_out= $_GET['fx_out'];
+	$showpost= $_GET['showpost'];
+?>
+
+function jquery_archive_list_animate(clicked_obj){
+	if(jQuery(clicked_obj).parent().children('ul').children('li').is(':hidden')){
+		jQuery(clicked_obj).parent().children('ul').children('li').<?php echo $fx_in ?>();
+		jQuery(clicked_obj).parent().children('a').children('.jaw_symbol').html('<?php echo $con_sym ?>');
+	}
+	else
+	{
+		jQuery(clicked_obj).parent().children('a').children('.jaw_symbol').html('<?php echo $ex_sym ?>');
+		jQuery(clicked_obj).parent().children('ul').children('li').<?php echo $fx_out ?>();
+	}
+	jQuery(clicked_obj).parent().toggleClass('expanded');
+}
+
+jQuery(document).ready(function() {
+	jQuery('li.jaw_years a.jaw_years').bind('click', function(){
+		jquery_archive_list_animate(this);
+                return false;
+	});
+
+	if(<?php echo $showpost ?>){
+		jQuery('li.jaw_months a.jaw_months').bind('click', function(){
+			jquery_archive_list_animate(this);
+                        return false;
+		});
+	}
+});
